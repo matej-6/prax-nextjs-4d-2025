8cc42498daf2daf856247a44db83ae4dd8d23e1b
Matej Barca <matejbarca2006@gmail.com>
2025-10-14 10:54:49 +0200

    finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,21 +21,22 @@ export default async function Home() {
         <p className="text-4xl font-bold">Spotify</p>
         <div className="grid grid-cols-2 gap-4">
           {albums.map((album) => (
-            <div key={album.id} className="card w-64 bg-base-100 shadow-sm">
+            <div
+              key={album.id}
+              className="card bg-base-100 w-64 card-xl shadow-sm"
+            >
               <div className="card-body">
-                <span className="badge badge-xs badge-warning">Pop</span>
-                <h2 className="text-3xl font-bold">{album.name}</h2>
+                <span className="badge badge-sm bg-emerald-200 text-emerald-800">
+                  Pop
+                </span>
+                <h2 className="text-3xl font-bold card-title">{album.name}</h2>
 
                 <p>ID: {album.id}</p>
-                <p>Author: {album.author_name}</p>
                 <p>
                   Release Date: {new Date(album.release_date).toDateString()}
                 </p>
                 <div className="mt-6">
-                  <Link
-                    className="btn btn-primary btn-block"
-                    href={`/album/${album.id}`}
-                  >
+                  <Link className="btn btn-block" href={`/album/${album.id}`}>
                     Detail
                   </Link>
                 </div>
